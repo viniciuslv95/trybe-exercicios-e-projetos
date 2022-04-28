2ce75b087cbe4ec739a06665e5781efb2f6109cd
viniciuslv95 <viniciusdelima_sep@hotmail.com>
2022-04-28 19:40:47 -0300

    Realiza exercicio 3

--- script.js
+++ script.js
@@ -17,4 +17,23 @@ for (let index = 0; index < word.length; index += 1) {
 console.log(wordInverter);
 
  // 3.
+ let array = ['java','javascript','python','html','css'];
+let maior = array[0];
+let menor = array[0];
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length > maior.length) {
+        maior = array[index];
+    }
+}
+console.log(maior);
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length < menor.length) {
+        menor = array[index];
+    }   
  
+    
+}
+console.log(menor);
+
+// 4.
