c7e462922ed2e3aee3de9d05fc1093c403f76e47
viniciuslv95 <viniciusdelima_sep@hotmail.com>
2022-04-28 17:43:05 -0300

    Faz exercicios 1 e 2

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1.
+let fatorial = 1;
+for (let index = 10; index > 0; index -= 1) {
+    fatorial *= index;
+    
+}
+console.log(fatorial);
+
+// 2.
+let word = 'tryber';
+let wordInverter = '';
+
+for (let index = 0; index < word.length; index += 1) {
+   wordInverter += word[word.length - 1 - index]
+    
+}
+console.log(wordInverter);
+
+ // 3.
+ 
