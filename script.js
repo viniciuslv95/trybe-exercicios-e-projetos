db67d7d060cc7cbfb50997a4c0d5592ca261b21a
viniciuslv95 <viniciusdelima_sep@hotmail.com>
2022-04-29 19:24:28 -0300

    Finaliza exercicios funções - exceto bônus

--- script.js
+++ script.js
@@ -70,8 +70,76 @@ leitor.livrosFavoritos.push(
 // console.log(leitor.livrosFavoritos);
 
 //8.
-console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
+// console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
 
 
 //                              FUNÇÕES                         //
 
+//1. palindromo
+
+//2. 
+function indexMaiorValor (array) {
+    let maiorValor = [0];
+    for (let index = 0; index < array.length; index += 1) {
+        if (array[maiorValor] > array[index]) {
+            maiorValor = index;
+        }
+        
+    }
+    return maiorValor;
+}
+// console.log(indexMaiorValor([2,3,6,7,10,1]));
+
+//3.
+function indexMenorValor (inteiros) {
+    let menorValor = [0];
+    for (let index = 0; index < inteiros.length; index += 1) {
+      if(inteiros[menorValor] >inteiros[index]) {
+          menorValor = index;
+      }
+        
+    }
+    return menorValor
+}
+// console.log(indexMenorValor([2,4,6,7,10,0,-3]));
+
+//4.
+function indicaMaiorNome (nomes) {
+    let nomeMaior = nomes[0];
+    for (let index in nomes){
+    if(nomeMaior.length < nomes[index].length) {
+        nomeMaior = nomes[index];
+    }
+    }
+        return nomeMaior
+    
+}
+// console.log(indicaMaiorNome(['José','Lucas','Nádia','Fernanda','Cairo','Joana']));
+
+//5. revisar, ainda não entendi
+
+//6.
+
+function somaNumeros (nNatural){
+    let valorSoma = 0;
+    for(let index = 1; index <= nNatural; index += 1){
+        valorSoma = valorSoma + index;
+    }
+    return valorSoma
+}
+// console.log(somaNumeros(5));
+
+//7.
+function verificaFinal(word, end) {
+    word = word.split('');
+    end = end.split('');
+    verificador = true;
+    for (let index = 0; index < end.length; index += 1) {
+      if (word[word.length - end.length + index] != end[index]) {
+        verificador = false;
+      }
+    }
+    return verificador;
+  }
+  
+//   console.log(verificaFinal('trybe','be'));
